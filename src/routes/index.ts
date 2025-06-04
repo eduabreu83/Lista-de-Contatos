b3e313da067f4dfcf7fad3edeafcbb48d8d01fe5
eduabreu83 <eduardoabreu83@gmail.com>
2025-06-04 14:23:35 -0300

    Update index.ts

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,7 +2,15 @@ import express from "express";
 
 const router = express.Router();
 
+router.post('/contato', (req, res) =>{
+  const { name } = req.body;
 
+  if (!name || name.length < 2) {
+    return res.json({ error: "Name is required" });
+  }
+
+  res.status(201).json({ contato: name  });
+});
 
 export default router;
-// This is a placeholder for the router. You can add your routes here.
+
