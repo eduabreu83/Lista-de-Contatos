82e6df0b3181b2cd98b05a924836b7ae4dcf2028
eduabreu83 <eduardoabreu83@gmail.com>
2025-06-09 16:52:26 -0300

    at

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,6 +1,5 @@
 import express from "express";
-import { readFile, writeFile } from "fs/promises";
-const dataSource = './data/list.txt';
+import { addContact, deleteContact, getContacts } from "../services/contacts";
 const router = express.Router();
 
 
@@ -10,44 +9,21 @@ router.post('/contato', async (req, res) => {
     return res.json({ error: "Name is required com pelo menos 2 caracteres" });
   }
 
-  let list: string[] = [];
-  try{
-    const data = await readFile(dataSource, { encoding: 'utf8' });
-    list = data.split('\n');
-  } catch (err) {}
-
-  list.push(name);
-  await writeFile(dataSource, list.join('\n'));
-
+  await addContact(name);
   res.status(201).json({ contato: name });
 });
 
 router.get('/contato', async (req, res) => {
-  let list: string[] = [];
-  try {
-    const data = await readFile(dataSource, { encoding: 'utf8' });
-    list = data.split('\n');
-  } catch (err) {}
-
+  let list = await getContacts();
   res.json({ contato: list });
 });
 
 router.delete('/contato', async (req, res) => {
   const { name } = req.query;
-if(!name) {
-    return res.status(400).json({ error: "Name is required" })
+  if (!name) {
+    return res.status(400).json({ error: "Name is required" });
   }
-
-  let list: string[] = [];
-  try {
-    const data = await readFile(dataSource, { encoding: 'utf8' });
-    list = data.split('\n');
-  } catch (err) { }
-
-  list = list.filter(item => item.toLowerCase() !== (name as string).toLowerCase());
-
-  await writeFile(dataSource, list.join('\n'));
-  res.status(200).json({ message: `Contato ${name} removido com sucesso` });
+  await deleteContact(name as string);
 
 });
 
---
+++ src/services/contacts.ts
@@ -0,0 +1,26 @@
+import { readFile, writeFile } from "fs/promises";
+
+const dataSource = './data/list.txt';
+
+export const getContacts = async () => {
+    let list: string[] = [];
+    try {
+        const data = await readFile(dataSource, { encoding: 'utf8' });
+        list = data.split('\n');
+    } catch (err) {
+        console.error("Error reading contacts:", err);
+    }
+    return list;
+}
+
+export const addContact = async (name: string) => { 
+    let list = await getContacts();
+    list.push(name);
+  await writeFile(dataSource, list.join('\n'));
+}
+
+export const deleteContact = async (name: string) => {
+    let list = await getContacts();
+    list = list.filter(item => item.toLowerCase() !== name.toLowerCase());
+    await writeFile(dataSource, list.join('\n'));
+}
